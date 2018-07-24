298adf1bcd89c779f17fdb13b20105679118bca4
WhiteDogLAPTOP <wodnr97@gmail.com>
2018-07-24 19:32:44 +0900

    스크립트 TestEnemyPlaneControl : Try2 함수 추가, RotateTowardTarget 함수 추가 - 기존 비행기를 회전하게 하는 구문을 함수화.

--- Assets/Scripts/Control/TestEnemyPlaneControl.cs
+++ Assets/Scripts/Control/TestEnemyPlaneControl.cs
@@ -6,7 +6,7 @@ public class TestEnemyPlaneControl : TestPlaneControl
 {
     //타겟이 있어야겠다.
     public GameObject targetToAttack;
-
+    public float fireRange;
     void Start()
     {
         targetToAttack = FindObjectOfType<PlayerPlane>().gameObject;
@@ -15,14 +15,7 @@ public class TestEnemyPlaneControl : TestPlaneControl
     // Update is called once per frame
     void Update()
     {
-
-        Try1();
-
-        /*2
-         * 플레이어를 한 번 공격하고 난 뒤엔 벽에 부딪히지 않게만 직진비행한다.
-         * 벽과 부딪힐 것 같으면 한바퀴 회전해 방향을 바꾸고 계속 직진비행한다.
-         * 직진비행 시간이 어느정도 끝나면 다시 플레이어를 쫒아온다.
-         */
+        Try2();
     }
     /*Try1 
         *앞으로 전진한다.
@@ -34,16 +27,47 @@ public class TestEnemyPlaneControl : TestPlaneControl
 
         if (targetToAttack != null)
         {
-            var relativeUp = targetToAttack.transform.TransformDirection(Vector3.forward);
-            var relativePos = targetToAttack.transform.position - transform.position;
-            /*
-            transform.rotation = Quaternion.Lerp(transform.rotation,
-                Quaternion.LookRotation(relativePos, transform.up),
-                rotateSpeed * Time.deltaTime);*/
-
-
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, relativePos, rotateSpeed * Time.deltaTime, 0.0f);
-            transform.rotation = Quaternion.LookRotation(newDir, this.transform.up);
+            
         }
+    }
+    /*2
+     * 앞으로 전진한다.
+     * 플레이어 방향으로 회전한다.
+     * 라인 트레이싱한다
+     * 라인 트레이싱 했을 때 플레이어가 잡히는 순간 PullTrigger해서 총 쏜다.
+     */
+     void Try2()
+    {
+        MoveForward();
+        RotateTowardTarget();
+        Debug.DrawRay(transform.position, transform.forward * fireRange, Color.red);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, fireRange))
+        {
+            if (hit.transform.CompareTag("Player"))
+                PullTrigger();
+        }
+    }
+    /*3
+     * 앞으로 전진한다
+     * 플레이어 방향으로 회전한다.
+     * 라인 트레이싱 한다.
+     * 라인 트레이싱 했을 때 플레이어가 잡히는 순간 PullTrigger를 한다.
+     * 시간이 될 때 마다 
+     */
+     void Try3()
+    { }
+
+    void RotateTowardTarget()
+    {
+        var relativeUp = targetToAttack.transform.TransformDirection(Vector3.forward);
+        var relativePos = targetToAttack.transform.position - transform.position;
+        /*
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+            Quaternion.LookRotation(relativePos, transform.up),
+            rotateSpeed * Time.deltaTime);*/
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, relativePos, rotateSpeed * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir, this.transform.up);
     }
 }
